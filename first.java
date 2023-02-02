cb0cab09c7eac9945bdb4604a7372cc60a50816e
Krishnakant Digole <krrishdigole@gmail.com>
2023-02-02 22:26:17 +0530

    the java code

---
+++ first.java
@@ -0,0 +1,13 @@
+class First{
+
+      public static void main(String[] args){
+            System.out.println("Hello corretto");
+}
+
+}
+
+class Second{
+      public static void main(String[] args){
+            System.out.println("Hello corretto");
+}
+}
